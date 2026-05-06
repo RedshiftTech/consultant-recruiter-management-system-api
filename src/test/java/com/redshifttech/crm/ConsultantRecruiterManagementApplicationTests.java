d30bc317002b8e8e258cc62e65d88a2f725af00d
Venkatesh Chilukuri <venkateswararaochilukuri10@gmail.com>
2026-05-06 14:49:45 -0500

    Backend API Setup

---
+++ src/test/java/com/redshifttech/crm/ConsultantRecruiterManagementApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.redshifttech.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultantRecruiterManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
